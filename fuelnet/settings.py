1bc09f36c77a317a159e3fa161aaf0b4175c49bc
codingrev <emmowu10@gmail.com>
2021-04-24 18:02:49 -0700

    petrolnet

--- fuelnet/settings.py
+++ fuelnet/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY=os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['petrolnet.herokuapp.com']
 
 
 # Application definition
